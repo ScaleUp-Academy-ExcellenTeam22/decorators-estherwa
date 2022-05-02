ca3be59d6a2b215460044e4209f2a730153a1cf2
estherwahnon <estherwa@hotmail.es>
2022-05-02 10:39:01 +0300

    surprise_dec ready for submitting

---
+++ surprise.py
@@ -0,0 +1,20 @@
+
+def surprise_decorator_func(func: callable) :
+    """
+    Making a decorator that is going to print “Surprise!” instead of its original value.
+    :param func: Function to call
+    :return: The function that returns another string in this case Surprise.
+    """
+    def decorate():
+        return "Surprise!"
+    return decorate
+
+#********************************************************************************************************************
+@surprise_decorator_func
+def function_basic() :
+    """
+    Returning one string.
+    :return: string.
+    """
+    return "print the function"
+#********************************************************************************************************************
