889e1fa4b9b0943fb0ee01b9974298c619073a3b
estherwahnon <estherwa@hotmail.es>
2022-05-02 23:28:00 +0300

    all exercise ready for submitting

--- main.py
+++ main.py
@@ -1,5 +1,20 @@
+from do_twice_decorator import print_name
 from surprise import function_basic
-
+from type_check import check_type
+#*********************************************************************************************************************
 if __name__ == '__main__':
+
+    #surprise_decorator
     print(function_basic())
+
+    #twice_decorator
+    print_name()
+
+    #type_check
+    print(check_type(2))
+    print( check_type("my exercise "))
+
+#********************************************************************************************************************
+
+
 
